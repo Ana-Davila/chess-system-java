1a3b710f3324956de7691ab707034c0006b7744b
Ana Davila Kulek <adkulek03@gmail.com>
2026-09-24 21:18:41 -0300

    Chess layer and printing the board

---
+++ src/chess/ChessPiece.java
@@ -0,0 +1,19 @@
+package chess;
+
+import chess_system.boardgame.Board;
+import chess_system.boardgame.Piece;
+
+public class ChessPiece extends Piece{
+	
+	private Color color;
+
+	public ChessPiece(Board board, Color color) {
+		super(board);
+		this.color = color;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+	
+}
--- src/chess_system/application/Program.java
+++ src/chess_system/application/Program.java
@@ -1,9 +1,11 @@
 package chess_system.application;
 
-import chess_system.boardgame.Board;
+import chess.ChessMatch;
 
 public class Program {
 	public static void main(String[] args) {
-		Board board = new Board(8, 8);
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
+		
 	}
 }
